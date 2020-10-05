4aedd50964c4025018df85c12894ae0a6f4a3062
[Harvis DeLeon] <[harvisd@hotmail.com]>
2020-10-05 03:38:23 -0400

    My Basic Web Calculator

---
+++ script.js
@@ -0,0 +1,8 @@
+                function addNumbers()
+                {
+                        var Num1 = parseInt(document.getElementById("Number1").value);
+                        var Num2 = parseInt(document.getElementById("Number2").value);
+                        var ansD = document.getElementById("answer");
+                        ansD.value = Num1 + Num2;
+                }
+      
